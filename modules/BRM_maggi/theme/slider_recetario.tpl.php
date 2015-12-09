e8702c2a6440f9733a505dd4e5b977193a8b079e
brm-solorzanoh <harold.lozano@brm.com.co>
2015-12-09 17:37:04 -0500

    add desarrollo slider recetario 8

--- modules/BRM_maggi/theme/slider_recetario.tpl.php
+++ modules/BRM_maggi/theme/slider_recetario.tpl.php
@@ -1,6 +1,8 @@
 <div class="cont-general">
 	<div class="cont-princ" title="<?php print_r($variables['nox'][0]->title) ?>" idVideo="<?php print_r($variables['nox'][0]->field_video_url['und'][0]['video_id']); ?>">
-		<iframe width="420" height="315" src="https://www.youtube.com/embed/<?php print_r($variables['nox'][0]->field_video_url['und'][0]['video_id']); ?>?rel=0&autoplay=0" frameborder="0" allowfullscreen></iframe>
+		<div class="embed-responsive embed-responsive-16by9">
+		  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?php print_r($variables['nox'][0]->field_video_url['und'][0]['video_id']); ?>?rel=0&autoplay=0"></iframe>
+		</div>
 	</div>
 	<div class="cont-secun">
 		<?php
@@ -8,7 +10,7 @@
 			foreach ($variables['nox'] as $value) {
 				if($aux>0){ ?>
 					<div class="cont-secun-item">
-						<img src="http://img.youtube.com/vi/<?php print_r($value->field_video_url['und'][0]['video_id']); ?>/0.jpg"/>
+						<img width="240px" height="180" src="http://img.youtube.com/vi/<?php print_r($value->field_video_url['und'][0]['video_id']); ?>/0.jpg"/>
 						<h3><?php print_r($value->title); ?></h3>
 					</div>
 				<?php }
