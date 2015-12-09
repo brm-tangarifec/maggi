813e33e05b5b8ddf8ae751d51d1f64b6e2814d3a
brm-solorzanoh <harold.lozano@brm.com.co>
2015-12-09 17:16:06 -0500

    add desarrollo slider recetario 3

--- modules/BRM_maggi/theme/slider_recetario.tpl.php
+++ modules/BRM_maggi/theme/slider_recetario.tpl.php
@@ -1,4 +1,23 @@
 <?php
-var_dump($variables);
-
+var_dump($variables['nox']);
 ?>
+<div class="cont-general">
+	<div class="cont-princ" title="<?php print_r($variables['nox'][0]->title) ?>" idVideo="<?php print_r($variables['nox'][0]->field_video_url['und'][0]['video_id']); ?>">
+		<?php
+			print_r($variables['nox'][0]->field_video_url['und'][0]['video_id']);
+		?>
+	</div>
+	<div class="cont-secun">
+		<?php
+			$aux = 0;
+			foreach ($variables['nox'] as $value) {
+				if($aux>0){ ?>
+					<div class="cont-secun-item">
+						<img src="http://img.youtube.com/vi/"<?php print_r($value->field_video_url['und'][0]['video_id']); ?>"/0.jpg"/>
+					</div>
+				<?php }
+				$aux++;
+			}
+		?>
+	</div>
+<div>
