62921c309bd80cd69c28f7ba315ed3e373a65485
Cristian Cortés <cristian.cortes@brm.com.co>
2015-12-10 19:01:32 -0500

    enrutado imgs categorias de producto

--- themes/maggiTheme/js/maggi.js
+++ themes/maggiTheme/js/maggi.js
@@ -99,7 +99,9 @@ jQuery(document).ready(function(){
 		bases = $("#quicktabs-tab-prodcutos_y_promociones-1").attr('href'),
 		caldos = $("#quicktabs-tab-prodcutos_y_promociones-2").attr('href'),
 		sazonadores = $("#quicktabs-tab-prodcutos_y_promociones-3").attr('href'),
-		contenedor = $("#quicktabs-prodcutos_y_promociones");
+		contenedor = $("#quicktabs-prodcutos_y_promociones"),
+		urlFiltro = window.location.pathname+window.location.search+window.location.hash,
+		port = window.location.port;
 
 	if ( url == "/productos-promociones" ){
 
@@ -107,69 +109,69 @@ jQuery(document).ready(function(){
 
 			contenedor.html('<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'+
 				sopas+'" class="producto-categoria">Sopas y Cremas</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/cremas-sopas.jpg" alt="Sopas y Cremas" title="Sopas y Cremas"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/cremas-sopas.jpg" alt="Sopas y Cremas" title="Sopas y Cremas"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
 				+bases+'" class="producto-categoria">Bases</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/bases.jpg" alt="Bases" title="Bases"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/bases.jpg" alt="Bases" title="Bases"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
 				+caldos+'" class="producto-categoria">Caldos</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/caldos.jpg" alt="Caldos" title="Caldos"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/caldos.jpg" alt="Caldos" title="Caldos"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
 				+sazonadores+'" class="producto-categoria">Sazonadores</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/sazonadores.jpg" alt="Sazonadores" title="Sazonadores"></div></div>');
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/sazonadores.jpg" alt="Sazonadores" title="Sazonadores"></div></div>');
 
-	}if ( url == "/productos-promociones?qt-prodcutos_y_promociones=0#qt-prodcutos_y_promociones" ){
+	}if ( urlFiltro == "/productos-promociones?qt-prodcutos_y_promociones=0#qt-prodcutos_y_promociones" ){
 
 			contenedor.addClass('productos').html("");
 
 			contenedor.html('<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto activo"><div class="content"><a href="'+
 				sopas+'" class="producto-categoria">Sopas y Cremas</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/cremas-sopas.jpg" alt="Sopas y Cremas" title="Sopas y Cremas"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/cremas-sopas.jpg" alt="Sopas y Cremas" title="Sopas y Cremas"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
 				+bases+'" class="producto-categoria">Bases</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/bases.jpg" alt="Bases" title="Bases"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/bases.jpg" alt="Bases" title="Bases"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
 				+caldos+'" class="producto-categoria"></a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/caldos.jpg" alt="Caldos" title="Caldos"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/caldos.jpg" alt="Caldos" title="Caldos"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
 				+sazonadores+'" class="producto-categoria"></a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/sazonadores.jpg" alt="Sazonadores" title="Sazonadores"></div></div>');
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/sazonadores.jpg" alt="Sazonadores" title="Sazonadores"></div></div>');
 
-	}if ( url == "/productos-promociones?qt-prodcutos_y_promociones=1#qt-prodcutos_y_promociones" ){
+	}if ( urlFiltro == "/productos-promociones?qt-prodcutos_y_promociones=1#qt-prodcutos_y_promociones" ){
 
 			contenedor.addClass('productos').html("");
 
 			contenedor.html('<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto "><div class="content"><a href="'+
 				sopas+'" class="producto-categoria">Sopas y Cremas</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/cremas-sopas.jpg" alt="Sopas y Cremas" title="Sopas y Cremas"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto activo"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/cremas-sopas.jpg" alt="Sopas y Cremas" title="Sopas y Cremas"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto activo"><div class="content"><a href="'
 				+bases+'" class="producto-categoria">Bases</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/bases.jpg" alt="Bases" title="Bases"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/bases.jpg" alt="Bases" title="Bases"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
 				+caldos+'" class="producto-categoria"></a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/caldos.jpg" alt="Caldos" title="Caldos"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/caldos.jpg" alt="Caldos" title="Caldos"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
 				+sazonadores+'" class="producto-categoria"></a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/sazonadores.jpg" alt="Sazonadores" title="Sazonadores"></div></div>');
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/sazonadores.jpg" alt="Sazonadores" title="Sazonadores"></div></div>');
 
-	}if ( url == "/productos-promociones?qt-prodcutos_y_promociones=2#qt-prodcutos_y_promociones" ){
+	}if ( urlFiltro == "/productos-promociones?qt-prodcutos_y_promociones=2#qt-prodcutos_y_promociones" ){
 
 			contenedor.addClass('productos').html("");
 
 			contenedor.html('<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto "><div class="content"><a href="'+
 				sopas+'" class="producto-categoria">Sopas y Cremas</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/cremas-sopas.jpg" alt="Sopas y Cremas" title="Sopas y Cremas"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto "><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/cremas-sopas.jpg" alt="Sopas y Cremas" title="Sopas y Cremas"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto "><div class="content"><a href="'
 				+bases+'" class="producto-categoria">Bases</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/bases.jpg" alt="Bases" title="Bases"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto activo"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/bases.jpg" alt="Bases" title="Bases"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto activo"><div class="content"><a href="'
 				+caldos+'" class="producto-categoria"></a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/caldos.jpg" alt="Caldos" title="Caldos"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/caldos.jpg" alt="Caldos" title="Caldos"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto"><div class="content"><a href="'
 				+sazonadores+'" class="producto-categoria"></a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/sazonadores.jpg" alt="Sazonadores" title="Sazonadores"></div></div>');
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/sazonadores.jpg" alt="Sazonadores" title="Sazonadores"></div></div>');
 
-	}if ( url == "/productos-promociones?qt-prodcutos_y_promociones=3#qt-prodcutos_y_promociones" ){
+	}if ( urlFiltro == "/productos-promociones?qt-prodcutos_y_promociones=3#qt-prodcutos_y_promociones" ){
 
 			contenedor.addClass('productos').html("");
 
 			contenedor.html('<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto "><div class="content"><a href="'+
 				sopas+'" class="producto-categoria">Sopas y Cremas</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/cremas-sopas.jpg" alt="Sopas y Cremas" title="Sopas y Cremas"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto "><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/cremas-sopas.jpg" alt="Sopas y Cremas" title="Sopas y Cremas"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto "><div class="content"><a href="'
 				+bases+'" class="producto-categoria">Bases</a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/bases.jpg" alt="Bases" title="Bases"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto "><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/bases.jpg" alt="Bases" title="Bases"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto "><div class="content"><a href="'
 				+caldos+'" class="producto-categoria"></a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/productos/caldos.jpg" alt="Caldos" title="Caldos"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto activo"><div class="content"><a href="'
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/productos/caldos.jpg" alt="Caldos" title="Caldos"></div></div><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 producto activo"><div class="content"><a href="'
 				+sazonadores+'" class="producto-categoria"></a><img src="'
-				+dominio+'/sites/all/themes/maggiTheme/files/images/sazonadores.jpg" alt="Sazonadores" title="Sazonadores"></div></div>');
+				+dominio+port+'/sites/all/themes/maggiTheme/files/images/sazonadores.jpg" alt="Sazonadores" title="Sazonadores"></div></div>');
 
 	}
 
