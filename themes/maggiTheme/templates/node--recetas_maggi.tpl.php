d976897361e74d933948f0409bb517ad7dba4034
brm-solorzanoh <harold.lozano@brm.com.co>
2015-12-16 20:00:26 -0500

    haspe add user register

--- themes/maggiTheme/templates/node--recetas_maggi.tpl.php
+++ themes/maggiTheme/templates/node--recetas_maggi.tpl.php
@@ -127,13 +127,12 @@ hide($content['links']);
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-lg-offset-3 col-md-offset-3 col-sm-offset-3">
       <div class="estrellas">
         <?php print render($content['field_receta_calificacion']);?>
-        <?php print render($content['links']);?>
         <span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star-empty"></span><span class="glyphicon glyphicon-star-empty"></span></div>
 
       <p class="extra-tools">
         <div class="botones-redes-wrapper"></div>
         <span class="icon icon-share"></span>
-        <span class="glyphicon glyphicon-heart-empty"></span>
+        <?php print render($content['links']);?>
       </p>
     </div>
     <!--/-Estrellas y Compartir-->
