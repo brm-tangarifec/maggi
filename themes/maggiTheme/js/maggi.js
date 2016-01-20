9e3055f6dccc9d25c0ec15aeeafd3c990762c452
cristian <cristian.tangarife@brm.com.co>
2016-01-20 17:41:34 -0500

    Se agrega script para prevenir el xsf

--- themes/maggiTheme/js/maggi.js
+++ themes/maggiTheme/js/maggi.js
@@ -7,6 +7,42 @@ $(document).ready(function(){
 
   jQuery('body.extra .recetas>div.row').addClass('u-bg-blanco');
 
+  /*FUncion para prevenir frame jacking*/
+
+/*
+Función enviada por nestlé "GLOBE"
+Content of bfbs.js
+=====================================================*/
+var __pcja_style = document.createElement('style');
+__pcja_style.type = 'text/css';
+__pcja_style.id = 'bfbs_cja';
+var __pcja_css = 'html{ display:none !important; }';
+if (__pcja_style.styleSheet){
+                __pcja_style.styleSheet.cssText = __pcja_css;
+	}else{
+                __pcja_style.appendChild(document.createTextNode(__pcja_css));
+          }
+
+document.getElementsByTagName("head")[0].appendChild(__pcja_style);
+if( self === top ){
+	                var __bfbs_cja = document.getElementById( 'bfbs_cja' );
+	                __bfbs_cja.parentNode.removeChild( document.getElementById( 'bfbs_cja' ) );
+	}else{
+                top.location = self.location;
+          }
+
+/*Función para verificar el dominio XSF*/
+try {
+if (top.location.hostname != self.location.hostname) throw 1;
+} catch (e) {
+top.location.href = self.location.href;
+}
+if (jQuery('form').length){
+	 //Ejecutar si existe el elemento
+	 jQuery('input,form').attr('autocomplete','off');
+	 //console.log('hola form');
+	}
+
 });
 
 /* Corazon Favoritos */
