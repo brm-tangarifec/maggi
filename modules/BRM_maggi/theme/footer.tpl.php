59cd9994adeb3c76213ec6c23c86b95b2924d290
cristian <cristian.tangarife@brm.com.co>
2016-02-01 15:23:15 -0500

    cambios en el modulo para habilitar conteo por email

--- modules/BRM_maggi/theme/footer.tpl.php
+++ modules/BRM_maggi/theme/footer.tpl.php
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
       <p class="text-center">
-        2016 MAGGI® Derechos Reservados / <a href="#">Política de Privacidad</a> | <a href="#">Aviso de Privacidad</a> | <a href="#">Política de Datos Personales </a>
+        2016 MAGGI® Derechos Reservados / <a href="http://prod.apps.nestle.com.co/nestle/politicasNestle/politicaDePrivacidad.pdf" target="_blank">Política de Privacidad</a> | <a href="http://prod.apps.nestle.com.co/nestle/politicasNestle/AvisodePrivacidadNestle.pdf" target="_blank">Aviso de Privacidad</a> | <a href="http://prod.apps.nestle.com.co/nestle/politicasNestle/PoliticaTratamientodeDatosPersonalesNestle.pdf" target="_blank">Política de Datos Personales </a>
         
       </p>
       <p class="text-center">
@@ -12,7 +12,7 @@
       <p class="text-center social-links">
         <span>Síguenos en:</span>
         
-        <a href="https://www.facebook.com/MaggiElSaborAColombia/" rel="nofollow" target="_blank" class="icon icon-fb"></a>
+        <a href="https://www.facebook.com/MaggiColombia/?fref=ts" rel="nofollow" target="_blank" class="icon icon-fb"></a>
         <a href="https://www.youtube.com/user/MAGGICOLOMBIA" rel="nofollow" target="_blank" class="icon icon-yt"></a>
         
         
